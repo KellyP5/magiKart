d6e66d3bc24ab15415f6a0bae7e8d37caf9bba88
Kelly <kellyjp09@gmail.com>
2020-10-24 22:45:42 -0500

    updates .js file

--- server/recipes.js
+++ server/recipes.js
@@ -1,10 +1,10 @@
-/**Spoontacular API Key: 274b0da100c3479bb8977f128718799f
-Initial recipe search
-https://api.spoonacular.com/recipes/complexSearch?query=dinner&number=1000&apiKey=274b0da100c3479bb8977f128718799f
 
-get Recipe information bulk
-https://api.spoonacular.com/recipes/informationBulk?ids=715538,716429
-**/
+
+
+//Keys
+const krogerGetKey = "&access_token=" + "eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJyZWNpcGVzLTk1MzM1OWQ0MmVjYjQwNzc1Mzg1OGRhNDM5MTUzODg2ODU4NTI5MzUyOTI0MDAwMDE4MiIsImV4cCI6MTYwMzU5Nzg5MCwiaWF0IjoxNjAzNTk2MDg1LCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6ImU2ZDdmNjVkLWJjMjAtNTkzMi05NjNlLTRlNDQyOWM0NzBiMSIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjAzNTk2MDkwOTM1NjE0MTI2LCJwZmN4IjoidXJuOmtyb2dlcmNvOnByb2ZpbGU6c2hvcHBlcjoxOTczNDBkNy0wYTQ1LTQzY2ItOTA3ZS04NDQ1ZDgyODk0ODEiLCJhenAiOiJyZWNpcGVzLTk1MzM1OWQ0MmVjYjQwNzc1Mzg1OGRhNDM5MTUzODg2ODU4NTI5MzUyOTI0MDAwMDE4MiJ9.J5poynhvUhVxSTuLccOWdl3Qv9ygUFaQYbc0VF74Us_qNUyPG661i5ORGIRaTq-4GDGR3PvoJrtjuyBhjjTDpZWrCVUVlnTG0mZBXkdfF80jhSC9Eu2m_xngbiiCxQ2VSrwls3YMgluCfQIosG00__1S1vrZsqOCa6C_xKjMeAq5SDCXKXeKLlRiFjvFyxU4IexQPFoxT1rKyLt_pQhvZxL1g0Ul1HVAVhekriGjBs-kCxJrVpM9sBEO9UXjI-O6pU0ebzk6pRwy3-rsYlIgFYO-rjbdCZ3dsdOrlOtHiF4CJQDBd43VWJFL1qYCMrtS-8bb1gmAytMwm_2DiQGJ_A";
+const krogerPutKey = "eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJyZWNpcGVzLTk1MzM1OWQ0MmVjYjQwNzc1Mzg1OGRhNDM5MTUzODg2ODU4NTI5MzUyOTI0MDAwMDE4MiIsImV4cCI6MTYwMzU5NzI0NywiaWF0IjoxNjAzNTk1NDQyLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6ImU2ZDdmNjVkLWJjMjAtNTkzMi05NjNlLTRlNDQyOWM0NzBiMSIsInNjb3BlIjoiY2FydC5iYXNpYzp3cml0ZSIsImF1dGhBdCI6MTYwMzU5NTQ0NzIwOTAwMDM4NCwicGZjeCI6InVybjprcm9nZXJjbzpwcm9maWxlOnNob3BwZXI6MTk3MzQwZDctMGE0NS00M2NiLTkwN2UtODQ0NWQ4Mjg5NDgxIiwiYXpwIjoicmVjaXBlcy05NTMzNTlkNDJlY2I0MDc3NTM4NThkYTQzOTE1Mzg4Njg1ODUyOTM1MjkyNDAwMDAxODIifQ.HgHz-GNAcz9rO4T-O5U51zlvjKcA7ITzoIDqXtkI4yy4GOkZxmboYh903Xosnu1NMeiA_ne-DhgAyBI3_86CJzoyvqMDZAIK0YAJNdjGU5HmnAR6hu6VnvXNS8vU5-42ky63yobs5vBL6uq2m71Gcupvx0BgUWO4Tdf-f5L6nlt0Z2_PeDw4uCD9BnI8ocdZsAAERtL3KZdiQ0eTyhqpYQv3lFuvBlMxYGgUy6-0tAJtN6bllG89wSZ1NerXYmVGxcATKpEXizDNRr_vdDTMvYPRFiGJlCgQZjaOBqlTdvrPzt9BcwT4gWFha-JbcdAiv1YxLsTQGyAfCenkOzoKfA";
+const spoonKey = "b0ed6fb01427453f9c80d75b1afb3524";
 
 const maleCalories = 1800;
 const femaleCalories = 1200;
@@ -18,23 +18,21 @@ var allIngredients;
 var productId;
 var productIds;
 var productIdArr = [""];
-const krogerGet = "https://api.kroger.com/v1/products?filter.term="
-const krogerKey = "&access_token=eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJyZWNpcGVzLTk1MzM1OWQ0MmVjYjQwNzc1Mzg1OGRhNDM5MTUzODg2ODU4NTI5MzUyOTI0MDAwMDE4MiIsImV4cCI6MTYwMzU5NDkyMiwiaWF0IjoxNjAzNTkzMTE3LCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6ImU2ZDdmNjVkLWJjMjAtNTkzMi05NjNlLTRlNDQyOWM0NzBiMSIsInNjb3BlIjoicHJvZHVjdC5jb21wYWN0IiwiYXV0aEF0IjoxNjAzNTkzMTIyNDk5NjU4ODQ4LCJwZmN4IjoidXJuOmtyb2dlcmNvOnByb2ZpbGU6c2hvcHBlcjoxOTczNDBkNy0wYTQ1LTQzY2ItOTA3ZS04NDQ1ZDgyODk0ODEiLCJhenAiOiJyZWNpcGVzLTk1MzM1OWQ0MmVjYjQwNzc1Mzg1OGRhNDM5MTUzODg2ODU4NTI5MzUyOTI0MDAwMDE4MiJ9.GY3e85qCZVYul59JScrUKUqJ5i_XrH_S4_qjoItkFm3Qz04XXtaRa2QKDUtps-Ut_zO7fjnCyJhbXc8R3A6nUEYBT18K8e1Row82CmiFoYWqlwWule1A9oRYoM7426fVSgAuIte2_F80_uxKYAcU0U1wcH4GyMXh0OSYCh2upHg4zESD1pxer_Yd3iSSothK-FakMMeYgLg5btTCR-lS1s9Kzbfs0bNjHhCIlzXWi2l0pWsf4bJ5HNElMAY-uwfHA9E2xEeo3r17rN3AFp-XFMDUE4_xCtwVxO9MZyq2zub6N8eXJvOSbXA-vv6C6p0b7xUMn0q7rKprlplO_7T5rA";
-const krogerCart = "https://api.kroger.com/v1/cart/add";
-const krogerCartKey = "";
-const spoonKey = "274b0da100c3479bb8977f128718799f";
+const krogerGet = "https://api.kroger.com/v1/products?filter.term=";
 var informationBulk = "https://api.spoonacular.com/recipes/informationBulk?ids=";
 var calorieSearch = "https://api.spoonacular.com/recipes/findByNutrients?minCalories=";
 let totalCalories;
 
+//var axios = require('axios');
 var data = JSON.stringify({"items":[{"quantity":1,"upc":"0001111060903"}]});
+
 var config = {
   method: 'put',
-  url: 'https://api.kroger.com/v1/cart/add',
+  url: 'https://cors-anywhere.herokuapp.com/' + 'https://api.kroger.com/v1/cart/add',
   headers: { 
     'Content-Type': 'application/json', 
-    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJtYWdpY2thcnQtZmFiNzE4OGQ4YmUyNTAxZWIzOWQ3NDVjN2ZkNGM5ZDMyNzA3NjQ0NjEyOTkxNjA5NzM3IiwiZXhwIjoxNjAzNTk0MzI3LCJpYXQiOjE2MDM1OTI1MjIsImlzcyI6ImFwaS5rcm9nZXIuY29tIiwic3ViIjoiZTZkN2Y2NWQtYmMyMC01OTMyLTk2M2UtNGU0NDI5YzQ3MGIxIiwic2NvcGUiOiJjYXJ0LmJhc2ljOndyaXRlIiwiYXV0aEF0IjoxNjAzNTkyNTI3NjY0MjIyNzQyLCJwZmN4IjoidXJuOmtyb2dlcmNvOnByb2ZpbGU6c2hvcHBlcjoxOTczNDBkNy0wYTQ1LTQzY2ItOTA3ZS04NDQ1ZDgyODk0ODEiLCJhenAiOiJtYWdpY2thcnQtZmFiNzE4OGQ4YmUyNTAxZWIzOWQ3NDVjN2ZkNGM5ZDMyNzA3NjQ0NjEyOTkxNjA5NzM3In0.Pl9t0piai5wQJnwbAz-q_Q3s74fx0194jCRfwNRLwO94nwEAlkB4KAlL8PIl2li6eF_W6axbfh7JRKy_5JfkpUOn7Y_3wmCe01HQJLqp3iHhH3Ea6rPfQohBEl8QPZFlcOn7HDSD3CG2GESdz08drL813SgjtnRCUvNDh-pzI5Ov8PEApvAdypkJcreaiHtaozxL25VuakQF7fkUHwf_xbbQDr2XbqGfbAIdg88o3GEREMin7Z2HMjq4ApyLm3GC74T1DBl6ENps1gIdJrk5oQUPXoGSTtO2HMC5N2miXDNsBNmWUNQ3HulAbVxoaRC3SBX-hX5DMr31VqBnqGwTiA', 
-    'Cookie': 'bm_sz=05E85F6EF3B629E52F395A76CD260DC9~YAAQIivorHaDlll1AQAAa0+KXQnSxZeh9YFFv0FYubazXYRH9OCoz1ql1nc4kkBd0zCh/Lx3cl8w0DDYdHfqpskTDpMgUe1pBAdDYBemkUORu/MoJufF0j7R6nG7FBP/3kgfSYj/FJjXbgiUqVKnQqOwsKBHyZS4BwEggBIoo4sdytiqoUvAbev1zHnVMLLk; s_id=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWRpcmVjdF91cmkiOiJodHRwOi8vMTI3LjAuMC4xOjU1MDAvc2VydmVyL2luZGV4Lmh0bWwiLCJzY29wZSI6ImNhcnQuYmFzaWM6d3JpdGUiLCJhdWQiOiJtYWdpY2thcnQtZmFiNzE4OGQ4YmUyNTAxZWIzOWQ3NDVjN2ZkNGM5ZDMyNzA3NjQ0NjEyOTkxNjA5NzM3IiwiZXhwIjoxNjAzNTkzMTAxLCJpc3MiOiJLcm9nZXIiLCJzdWIiOiIyMzFjYjIyNC1hZGQ4LTQ1NGEtYjU1Yy0wMDk1MDgzNGE2YzMifQ.t6ZqyMJ34Znzy4Avjk5jTzvGr4pMSFBcN54LP7ejGhY; akaalb_Digital_ALB_API=~op=KT_Digital_API_KCVG_F5:api-kcvg|KT_Digital_API_Apigee:api-apigee|~rv=68~m=api-kcvg:0|api-apigee:0|~os=75b4a9ec926d2a9e67035451773cec6c~id=32fb16e70b8fbcf2b139943e3399c5a1; pid=231cb224-add8-454a-b55c-00950834a6c3; s_ips=575; s_tslv=1603592501717; AMCVS_371C27E253DB0F910A490D4E%40AdobeOrg=1; s_cc=true; _uetsid=d1df7dd0166811eb84aeabc726aa02de; _uetvid=d1dfaae0166811eb90e0896cc828d177; AMCV_371C27E253DB0F910A490D4E%40AdobeOrg=-432600572%7CMCIDTS%7C18561%7CMCMID%7C72025143155091291700140402671523222818%7CMCAAMLH-1604197301%7C7%7CMCAAMB-1604197301%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1603599701s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-18568%7CvVersion%7C4.5.2; _gcl_au=1.1.116707699.1603592502; s_tp=1172; s_ppv=bn%253Av1%253Aconnect%253Asignin%2C49%2C49%2C575%2C1%2C2; s_sq=krgrmobileprod%3D%2526c.%2526a.%2526activitymap.%2526page%253Dbn%25253Av1%25253Aconnect%25253Asignin%2526link%253DSign%252520In%2526region%253Dsignin_form%2526pageIDType%253D1%2526.activitymap%2526.a%2526.c%2526pid%253Dbn%25253Av1%25253Aconnect%25253Asignin%2526pidt%253D1%2526oid%253DSign%252520In%2526oidt%253D3%2526ot%253DSUBMIT; umid=ee1801cb-f7d9-46ad-b174-20af4dff7b6d; j_id=; _abck=F7379DFDA22C678310CB45EFF2CB1514~-1~YAAQlivorJq2/VV1AQAA/i6QXQS12bmRq/wZfEI/b/U4DFgSNpWoZ2wCNAdH5Ae3Vrtwu19aiAxA0ognoPYX6NHjiEZvlnGT7FbSedeltD3gyXFAyYQcJXQ7/RK8qxGHu056dxF5O5YIoc1cqv1Am42DEuAtiUPjSASTHD2dHpTIQdElTu9BlMFGOXvcvVVUE4PkiPaRUgpOzJNEubBE9i8/oErVz7FI15joWVT0gv/TNbO20SID8Zk8QFstqfhZ9XHhtpsHVLhv2jrWeYpPyTvsyZC2XvkAgU3rXAZFTGjRSqjwZMEpVXqFomlsREoIaapiDJ99vUYOvIsCdlLaCoWoEaA=~0~-1~-1'
+    'Authorization': 'Bearer ' + krogerPutKey, 
+    'Cookie': '_gcl_au=1.1.2006088855.1603575799; origin=fldc; rxVisitor=1603575845111TT5N6IV68MGRL1B5MC4MI3P8CH0ICPNU; DivisionID=025; StoreCode=00613; _pin_unauth=dWlkPU5qRm1NR1JqWW1JdE5EZ3lZaTAwTVRCakxUbGpaamd0WWpReE1USTJOR1UzT1RjNQ; RT="z=1&dm=kroger.com&si=gp9z3jo8j6b&ss=kgo7nl1g&sl=1&tt=0&nu=9da7d33a729e95e92719ecd580697b07&cl=pja&obo=1&ld=qys&r=9da7d33a729e95e92719ecd580697b07&ul=qyu&hd=rhb"; akaalb_Digital_ALB_API=~op=KT_Digital_API_KCVG_F5:api-kcvg|KT_Digital_API_Apigee:api-apigee|~rv=37~m=api-kcvg:0|api-apigee:0|~os=75b4a9ec926d2a9e67035451773cec6c~id=91501d3e41a0b7dcff31146f9a95ee0f; s_ips=578; AMCVS_371C27E253DB0F910A490D4E%40AdobeOrg=1; s_cc=true; bm_sz=E5EF5A9A2AFF595879C3C8AF26FFC723~YAAQTqQ2FzcDWFd1AQAA5rFWXQlhLiA+WKc6ySuRccSX2f81xdsR/kkchsW2bWV6ujWAluwD/ijYLgAxMfvj8teHskR9JfbX3c2vkHaalJp2UgqtjKtPEGKNS6Xg1wpHeLnNeQ5Fe2+tA/zgXDDo7p7Ww2tyeUJ7y69phXO6wE3wY5217TMne/6ch62+7ekY; AMCV_371C27E253DB0F910A490D4E%40AdobeOrg=-432600572%7CMCIDTS%7C18560%7CMCMID%7C36280152519877809193001074290303296431%7CMCAAMLH-1604200128%7C7%7CMCAAMB-1604200128%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1603602528s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-18567%7CvVersion%7C4.5.2; s_id=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWRpcmVjdF91cmkiOiJodHRwOi8vMTI3LjAuMC4xOjU1MDAvaW5kZXguaHRtbCIsInNjb3BlIjoiY2FydC5iYXNpYzp3cml0ZSIsImF1ZCI6InJlY2lwZXMtOTUzMzU5ZDQyZWNiNDA3NzUzODU4ZGE0MzkxNTM4ODY4NTg1MjkzNTI5MjQwMDAwMTgyIiwiZXhwIjoxNjAzNTk2MDM5LCJpc3MiOiJLcm9nZXIiLCJzdWIiOiIxNmE2YzA3YS0xN2JkLTRjZjUtYmExMi04ZmU2ZGJjMTdlNDIifQ.3oKxChsJZx0lAxxDj24HhMpkPPw92PLDUd0QyTVcMNg; pid=16a6c07a-17bd-4cf5-ba12-8fe6dbc17e42; s_tslv=1603595440090; _uetsid=ee2040f0164111ebbb72f399747a8f42; _uetvid=ee205d00164111eb9fafe9bb46652adc; s_tp=1172; s_ppv=bn%253Av1%253Aconnect%253Asignin%2C49%2C49%2C578%2C1%2C2; s_sq=krgrmobileprod%3D%2526c.%2526a.%2526activitymap.%2526page%253Dbn%25253Av1%25253Aconnect%25253Asignin%2526link%253DSign%252520In%2526region%253Dsignin_form%2526pageIDType%253D1%2526.activitymap%2526.a%2526.c%2526pid%253Dbn%25253Av1%25253Aconnect%25253Asignin%2526pidt%253D1%2526oid%253DSign%252520In%2526oidt%253D3%2526ot%253DSUBMIT; umid=86c5f2d1-5d43-450b-81c6-46934e25b8f5; j_id=; _abck=138A8FB4A14A4FFFD0318F8C46124B27~-1~YAAQZ6Q2F7xzait1AQAAab28XQTnqh8ebYc8AvSXkfUOH5B94dW09wirGXLLr52SCjIJ2OknGUU7etqc7nJGJmNmswTZ2/fWdL8a+J/LDbeJnha7vv2GAu6eJJSdgmU0blLNxoM8gPtnpwaalfzwnlkopMDEouetCzbXZ8rLPcc9bBPrZhfEiYI0+ZoJvgVL2rCWBlemWXCL912rpEc4oAGg78n/iGZze3baE3Ff1Z/zE8QGaVILQLUtmoLUc2hi3uPsHFUi5cH9hDo5tnH/IQy+yyto6abUZxpfd+PpTejkuOQ4n3AsUvsZcywvWvr9E7BMmdXX1o4ZYnRGbvKi8rj4/ds=~0~-1~-1'
   },
   data : data
 };
@@ -92,15 +90,29 @@ function getIngredients() {
 function printAllIngredients() {
     ingredientList.forEach(element => {
         //console.log(krogerGet + element + krogerKey);
-        axios.get(krogerGet + element + krogerKey)
+        axios.get(krogerGet + element + krogerGetKey)
         .then(function (response) {
             productId = response.data.data[0].productId;
             productIdArr.push(productId);
-            /**axios(config)
-            .then(function (response) {
-                console.count(response);
-            });**/
+            data = JSON.stringify({"items":[{"quantity":1,"upc":productId}]});
+            config = {
+                method: 'put',
+                url: 'https://cors-anywhere.herokuapp.com/' + 'https://api.kroger.com/v1/cart/add',
+                headers: { 
+                  'Content-Type': 'application/json', 
+                  'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJyZWNpcGVzLTk1MzM1OWQ0MmVjYjQwNzc1Mzg1OGRhNDM5MTUzODg2ODU4NTI5MzUyOTI0MDAwMDE4MiIsImV4cCI6MTYwMzU5NzI0NywiaWF0IjoxNjAzNTk1NDQyLCJpc3MiOiJhcGkua3JvZ2VyLmNvbSIsInN1YiI6ImU2ZDdmNjVkLWJjMjAtNTkzMi05NjNlLTRlNDQyOWM0NzBiMSIsInNjb3BlIjoiY2FydC5iYXNpYzp3cml0ZSIsImF1dGhBdCI6MTYwMzU5NTQ0NzIwOTAwMDM4NCwicGZjeCI6InVybjprcm9nZXJjbzpwcm9maWxlOnNob3BwZXI6MTk3MzQwZDctMGE0NS00M2NiLTkwN2UtODQ0NWQ4Mjg5NDgxIiwiYXpwIjoicmVjaXBlcy05NTMzNTlkNDJlY2I0MDc3NTM4NThkYTQzOTE1Mzg4Njg1ODUyOTM1MjkyNDAwMDAxODIifQ.HgHz-GNAcz9rO4T-O5U51zlvjKcA7ITzoIDqXtkI4yy4GOkZxmboYh903Xosnu1NMeiA_ne-DhgAyBI3_86CJzoyvqMDZAIK0YAJNdjGU5HmnAR6hu6VnvXNS8vU5-42ky63yobs5vBL6uq2m71Gcupvx0BgUWO4Tdf-f5L6nlt0Z2_PeDw4uCD9BnI8ocdZsAAERtL3KZdiQ0eTyhqpYQv3lFuvBlMxYGgUy6-0tAJtN6bllG89wSZ1NerXYmVGxcATKpEXizDNRr_vdDTMvYPRFiGJlCgQZjaOBqlTdvrPzt9BcwT4gWFha-JbcdAiv1YxLsTQGyAfCenkOzoKfA', 
+                  'Cookie': '_gcl_au=1.1.2006088855.1603575799; origin=fldc; rxVisitor=1603575845111TT5N6IV68MGRL1B5MC4MI3P8CH0ICPNU; DivisionID=025; StoreCode=00613; _pin_unauth=dWlkPU5qRm1NR1JqWW1JdE5EZ3lZaTAwTVRCakxUbGpaamd0WWpReE1USTJOR1UzT1RjNQ; RT="z=1&dm=kroger.com&si=gp9z3jo8j6b&ss=kgo7nl1g&sl=1&tt=0&nu=9da7d33a729e95e92719ecd580697b07&cl=pja&obo=1&ld=qys&r=9da7d33a729e95e92719ecd580697b07&ul=qyu&hd=rhb"; akaalb_Digital_ALB_API=~op=KT_Digital_API_KCVG_F5:api-kcvg|KT_Digital_API_Apigee:api-apigee|~rv=37~m=api-kcvg:0|api-apigee:0|~os=75b4a9ec926d2a9e67035451773cec6c~id=91501d3e41a0b7dcff31146f9a95ee0f; s_ips=578; AMCVS_371C27E253DB0F910A490D4E%40AdobeOrg=1; s_cc=true; bm_sz=E5EF5A9A2AFF595879C3C8AF26FFC723~YAAQTqQ2FzcDWFd1AQAA5rFWXQlhLiA+WKc6ySuRccSX2f81xdsR/kkchsW2bWV6ujWAluwD/ijYLgAxMfvj8teHskR9JfbX3c2vkHaalJp2UgqtjKtPEGKNS6Xg1wpHeLnNeQ5Fe2+tA/zgXDDo7p7Ww2tyeUJ7y69phXO6wE3wY5217TMne/6ch62+7ekY; AMCV_371C27E253DB0F910A490D4E%40AdobeOrg=-432600572%7CMCIDTS%7C18560%7CMCMID%7C36280152519877809193001074290303296431%7CMCAAMLH-1604200128%7C7%7CMCAAMB-1604200128%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1603602528s%7CNONE%7CMCAID%7CNONE%7CMCSYNCSOP%7C411-18567%7CvVersion%7C4.5.2; s_id=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWRpcmVjdF91cmkiOiJodHRwOi8vMTI3LjAuMC4xOjU1MDAvaW5kZXguaHRtbCIsInNjb3BlIjoiY2FydC5iYXNpYzp3cml0ZSIsImF1ZCI6InJlY2lwZXMtOTUzMzU5ZDQyZWNiNDA3NzUzODU4ZGE0MzkxNTM4ODY4NTg1MjkzNTI5MjQwMDAwMTgyIiwiZXhwIjoxNjAzNTk2MDM5LCJpc3MiOiJLcm9nZXIiLCJzdWIiOiIxNmE2YzA3YS0xN2JkLTRjZjUtYmExMi04ZmU2ZGJjMTdlNDIifQ.3oKxChsJZx0lAxxDj24HhMpkPPw92PLDUd0QyTVcMNg; pid=16a6c07a-17bd-4cf5-ba12-8fe6dbc17e42; s_tslv=1603595440090; _uetsid=ee2040f0164111ebbb72f399747a8f42; _uetvid=ee205d00164111eb9fafe9bb46652adc; s_tp=1172; s_ppv=bn%253Av1%253Aconnect%253Asignin%2C49%2C49%2C578%2C1%2C2; s_sq=krgrmobileprod%3D%2526c.%2526a.%2526activitymap.%2526page%253Dbn%25253Av1%25253Aconnect%25253Asignin%2526link%253DSign%252520In%2526region%253Dsignin_form%2526pageIDType%253D1%2526.activitymap%2526.a%2526.c%2526pid%253Dbn%25253Av1%25253Aconnect%25253Asignin%2526pidt%253D1%2526oid%253DSign%252520In%2526oidt%253D3%2526ot%253DSUBMIT; umid=86c5f2d1-5d43-450b-81c6-46934e25b8f5; j_id=; _abck=138A8FB4A14A4FFFD0318F8C46124B27~-1~YAAQZ6Q2F7xzait1AQAAab28XQTnqh8ebYc8AvSXkfUOH5B94dW09wirGXLLr52SCjIJ2OknGUU7etqc7nJGJmNmswTZ2/fWdL8a+J/LDbeJnha7vv2GAu6eJJSdgmU0blLNxoM8gPtnpwaalfzwnlkopMDEouetCzbXZ8rLPcc9bBPrZhfEiYI0+ZoJvgVL2rCWBlemWXCL912rpEc4oAGg78n/iGZze3baE3Ff1Z/zE8QGaVILQLUtmoLUc2hi3uPsHFUi5cH9hDo5tnH/IQy+yyto6abUZxpfd+PpTejkuOQ4n3AsUvsZcywvWvr9E7BMmdXX1o4ZYnRGbvKi8rj4/ds=~0~-1~-1'
+                },
+                data : data
+              };
             console.log(productId);
+            axios(config)
+                .then(function (response) {
+                    console.log(JSON.stringify(response.data));
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         })
         .catch(function (error) {
             console.log(error);
@@ -108,10 +120,6 @@ function printAllIngredients() {
     });
 }
 
-axios(config)
-        .then(function (response) {
-            console.count(response);
-    });
 
 //Uncomment to perform all of the API calls
-//etRecipes();
+//getRecipes();
